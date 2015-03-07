a4f6118ea4d6bb4a8354bd8a3b799379c53246de
David R. Poindexter III <drpoindexter@gmail.com>
2015-03-07 09:49:21 -0500

    List static json students

--- app/students.js
+++ app/students.js
@@ -2,7 +2,13 @@
     'use strict';
     angular.module('ngGradebookApp')
         .controller('Students', ['$scope', function ($scope) {
-            $scope.pageTitle = 'Hello World';
+            $scope.students = [
+              {firstName: 'Joe', lastName: 'Schmoe', grade: 50},
+              {firstName: 'Michael ', lastName: 'Lieberman', grade: 97},
+              {firstName: 'Steven ', lastName: 'Romano', grade: 80},
+              {firstName: 'Patricia ', lastName: 'Rundell', grade: 95},
+              {firstName: 'Olive ', lastName: 'Castleman', grade: 100}
+            ];
 
         }]);
 })();
